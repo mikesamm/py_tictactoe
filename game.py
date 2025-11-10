9543fbafa54b6198586bdf40765e95380257f5c7
Mike Sammartino <sammartino.mike@gmail.com>
2025-11-10 13:16:49 -0600

    two hour progress, about to implement valid move check

--- game.py
+++ game.py
@@ -7,14 +7,29 @@
 #       print(square, end=',')
 #     print()
 
+
+# LEAVING OFF WITH GETTING THE PLAYER INPUT AND CHECKING FOR VALIDITY, NEEDS TO BE LOOP IF NOT VALID
+
+def get_player_input():
+    player_input = input(f"Player {current_player}, what's your move?: ")
+    # TODO: check player input
+    match = False
+    # iterate through game_grid for matching number
+    for i in range(len(game_grid)):
+      for j in range(len(game_grid[i])):
+        if game_grid[i][j] == player_input:
+          # place current_player mark on square
+          match = True
+          game_grid[i][j] = f'{current_player}'
+
 def game_loop():
   game_grid = [ ['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
 
   test_loops = 0
+  # set first current player
   current_player = 'X'
 
   while test_loops <= 5:
-    # set current player
     # print game_grid
     # print_grid()
     # iterate through game_grid to print latest game grid
@@ -22,18 +37,23 @@ def game_loop():
       for square in row:
         print(square, end=',')
       print()
-    # check win or draw condition to end game loop
+    # TODO: check win or draw condition to end game loop
     # ask for player input
-    # print(f"Player {current_player}, what's your move?")
     # player inputs number for open square
-    player_input = input(f"Player {current_player}, what's your move?: ")
-    # iterate through game_grid for matching number
-    for i in range(len(game_grid)):
-      for j in range(len(game_grid[i])):
-        if game_grid[i][j] == player_input:
-          # place current_player mark on square
-          game_grid[i][j] = f'{current_player}'
-    print(f"Player {current_player} moved here: " + player_input + '\n')
+
+    get_player_input()
+    # player_input = input(f"Player {current_player}, what's your move?: ")
+    # # TODO: check player input
+    # match = False
+    # # iterate through game_grid for matching number
+    # for i in range(len(game_grid)):
+    #   for j in range(len(game_grid[i])):
+    #     if game_grid[i][j] == player_input:
+    #       # place current_player mark on square
+    #       match = True
+    #       game_grid[i][j] = f'{current_player}'
+
+    print('\n')
     # change current_player
     if current_player == 'X':
       current_player = 'O'
