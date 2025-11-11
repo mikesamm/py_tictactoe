d02d7afa5ae2272d401830760ac7880c8cc4d9fd
Mike Sammartino <sammartino.mike@gmail.com>
2025-11-11 09:13:38 -0600

    init storage lists for player moves, add moves to respective lists

--- game.py
+++ game.py
@@ -1,5 +1,17 @@
-game_grid = [ ['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+game_grid = [
+  ['1', '2', '3'],
+  ['4', '5', '6'],
+  ['7', '8', '9']
+]
 
+# player move storage lists
+x_squares = []
+o_squares = []
+
+# set first current player
+current_player = 'X'
+
+is_game_ended = False
 
 # def print_grid():
 #   # iterate through game_grid to print latest game grid
@@ -8,15 +20,13 @@ game_grid = [ ['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
 #       print(square, end=',')
 #     print()
 
-# set first current player
-current_player = 'X'
-is_game_ended = False
-
 # get and check player input
 def get_player_input():
   match = False
+
   while match == False:
     player_input = input(f"Player {current_player}, what's your move?: ")
+
     # iterate through game_grid for matching number
     for i in range(len(game_grid)):
       for j in range(len(game_grid[i])):
@@ -24,10 +34,33 @@ def get_player_input():
           match = True
           # place current_player mark on square
           game_grid[i][j] = f'{current_player}'
+          # add player choice in respective storage list
+          if globals()["current_player"] == 'X':
+            globals()["x_squares"].append(player_input)
+          else:
+            globals()["o_squares"].append(player_input)
+
     if match == False:
       print("Please pick a valid square.")
 
-def is_win_draw():
+def is_win():
+  # win: vertical, horizontal, or diagonal line is complete
+  # define win conditions
+  win_conditions = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['3', '6', '9'],
+    ['1', '5', '9'],
+    ['3', '5', '7']
+  ]
+  # check storage lists for winning combination
+  # does x_squares contain all 3 numbers in one of the winning conditions?
+  # does o_squares contain all 3 numbers in one of the winning conditions?
+
+def is_draw():
   # draw: no available squares left
   occupied_squares = 0
   for i in range(len(game_grid)):
@@ -38,10 +71,8 @@ def is_win_draw():
     print("Grab a sketch pad, because it's a draw...")
     globals()["is_game_ended"] = True
     exit()
-  # win: vertical, horizontal, or diagonal line is complete
 
 def game_loop():
-  # test_loops = 0
 
   while globals()["is_game_ended"] == False:
     # print game_grid
@@ -51,8 +82,9 @@ def game_loop():
       for square in row:
         print(square, end=',')
       print()
-    # TODO: check win or draw condition to end game loop
-    is_win_draw()
+
+    is_win()
+    is_draw()
 
     get_player_input()
     print('\n')
@@ -62,7 +94,5 @@ def game_loop():
       globals()["current_player"] = 'O'
     else:
       globals()["current_player"] = 'X'
-    # test_loops += 1
 
 game_loop()
-# upon win or draw, display message
